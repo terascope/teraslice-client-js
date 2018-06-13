90335cc5ba476f2dc0db6345c798c1d8939a5bf4
peterdemartini <thepeterdemartini@gmail.com>
2018-06-13 08:11:37 -0700

    add more tests and change the code to make it more logical

--- spec/jobs-spec.js
+++ spec/jobs-spec.js
@@ -1,31 +1,83 @@
 'use strict';
 
 const client = require('../');
+const Job = require('../lib/job');
 const nock = require('nock');
 
 describe('Teraslice Jobs', () => {
-    let jobs;
-
-    beforeEach(() => {
-        ({ jobs } = client({
-            host: 'teraslice.example.com'
-        }));
-        nock('teraslice.example.com');
-    });
-
     afterEach(() => {
         nock.cleanAll();
     });
 
-    it('should have the method submit', () => {
-        expect(typeof jobs.submit).toEqual('function');
-    });
+    describe('->submit', () => {
+        let scope;
+        let jobs;
 
-    it('should have the method list', () => {
-        expect(typeof jobs.list).toEqual('function');
-    });
+        beforeEach(() => {
+            ({ jobs } = client({
+                baseUrl: 'http://teraslice.example.dev'
+            }));
+            scope = nock('http://teraslice.example.dev');
+        });
 
-    it('should have the method wrap', () => {
-        expect(typeof jobs.wrap).toEqual('function');
+        describe('when submitting without a jobSpec', () => {
+            it('should fail', (done) => {
+                jobs.submit().then(fail).catch((err) => {
+                    expect(err.toString()).toEqual('Error: submit requires a jobSpec');
+                    done();
+                });
+            });
+        });
+
+        describe('when submitting with a valid jobSpec', () => {
+            let result;
+            beforeEach((done) => {
+                const jobSpec = {
+                    some_job: true,
+                    operations: [{ some: 'operation' }]
+                };
+                scope.post('/jobs', jobSpec)
+                    .query({ start: false })
+                    .reply(202, {
+                        job_id: 'some-job-id'
+                    });
+
+                jobs.submit(jobSpec)
+                    .then((_result) => {
+                        result = _result;
+                        done();
+                    }).catch(fail);
+            });
+
+            it('should resolve an instanceof a Job', () => {
+                expect(result instanceof Job).toBeTrue();
+            });
+        });
+
+        describe('when submitting without operations', () => {
+            let err;
+            beforeEach((done) => {
+                const jobSpec = {
+                    some_job: true
+                };
+                scope.post('/jobs', jobSpec)
+                    .query({ start: false })
+                    .reply(400, 'No job was posted');
+
+                jobs.submit(jobSpec)
+                    .then(fail).catch((_err) => {
+                        err = _err;
+                        done();
+                    });
+            });
+
+            it('should reject an instanceof a Error', () => {
+                expect(err instanceof Error).toBeTrue();
+            });
+
+            it('should have a message of No job was posted', () => {
+                expect(err.message).toEqual('No job was posted');
+            });
+        });
     });
 });
