233e3db79b626267a7d70a8db385f8be62eeb60b
peterdemartini <thepeterdemartini@gmail.com>
2018-06-15 09:47:50 -0700

    add more job tests

--- spec/job-spec.js
+++ spec/job-spec.js
@@ -66,4 +66,102 @@ describe('Teraslice Job', () => {
             });
         });
     });
+
+    describe('->start', () => {
+        describe('when called with nothing', () => {
+            let result;
+            beforeEach((done) => {
+                scope.post('/jobs/some-job-id/_start')
+                    .reply(200, {
+                        id: 'example'
+                    });
+
+                new Job({ baseUrl }, 'some-job-id')
+                    .start()
+                    .then((_result) => {
+                        result = _result;
+                        done();
+                    }).catch(fail);
+            });
+
+            it('should resolve json result from Teraslice', () => {
+                expect(result).toEqual({
+                    id: 'example'
+                });
+            });
+        });
+
+        describe('when called with a query', () => {
+            let result;
+            beforeEach((done) => {
+                scope.post('/jobs/some-job-id/_start')
+                    .query({ someParam: 'yes' })
+                    .reply(200, {
+                        key: 'some-other-key'
+                    });
+
+                new Job({ baseUrl }, 'some-job-id')
+                    .start({ someParam: 'yes' })
+                    .then((_result) => {
+                        result = _result;
+                        done();
+                    }).catch(fail);
+            });
+
+            it('should resolve json result from Teraslice', () => {
+                expect(result).toEqual({
+                    key: 'some-other-key'
+                });
+            });
+        });
+    });
+
+    ['start', 'stop', 'pause', 'resume', 'recover'].forEach((method) => {
+        describe(`->${method}`, () => {
+            describe('when called with nothing', () => {
+                let result;
+                beforeEach((done) => {
+                    scope.post(`/jobs/some-job-id/_${method}`)
+                        .reply(200, {
+                            id: 'example'
+                        });
+
+                    new Job({ baseUrl }, 'some-job-id')[method]()
+                        .then((_result) => {
+                            result = _result;
+                            done();
+                        }).catch(fail);
+                });
+
+                it('should resolve json result from Teraslice', () => {
+                    expect(result).toEqual({
+                        id: 'example'
+                    });
+                });
+            });
+
+            describe('when called with a query', () => {
+                let result;
+                beforeEach((done) => {
+                    scope.post(`/jobs/some-job-id/_${method}`)
+                        .query({ someParam: 'yes' })
+                        .reply(200, {
+                            key: 'some-other-key'
+                        });
+
+                    new Job({ baseUrl }, 'some-job-id')[method]({ someParam: 'yes' })
+                        .then((_result) => {
+                            result = _result;
+                            done();
+                        }).catch(fail);
+                });
+
+                it('should resolve json result from Teraslice', () => {
+                    expect(result).toEqual({
+                        key: 'some-other-key'
+                    });
+                });
+            });
+        });
+    });
 });
